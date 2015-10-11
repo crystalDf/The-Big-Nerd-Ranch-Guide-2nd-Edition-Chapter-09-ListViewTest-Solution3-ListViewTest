02df05bbe955e9dabf49ca1b37a481118dd03eb5
crystalDf <chendong333@gmail.com>
2015-10-11 18:13:04 +0800

    Chapter 09 ListViewTest solution3

---
+++ app/src/main/java/com/star/listviewtest/ImageAdapter.java
@@ -0,0 +1,80 @@
+package com.star.listviewtest;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+
+public class ImageAdapter extends ArrayAdapter<String> {
+
+    private ImageLoader mImageLoader;
+
+    public ImageAdapter(Context context, int resource, String[] objects) {
+        super(context, resource, objects);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        mImageLoader = new ImageLoader(requestQueue, new BitmapCache());
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        String currentUrl = getItem(position);
+
+        View view;
+
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.image_item, null);
+        } else {
+            view = convertView;
+        }
+
+        NetworkImageView networkImageView = (NetworkImageView)
+                view.findViewById(R.id.image_view);
+
+        networkImageView.setDefaultImageResId(R.drawable.empty_photo);
+        networkImageView.setErrorImageResId(R.drawable.empty_photo);
+        networkImageView.setImageUrl(currentUrl, mImageLoader);
+
+        return view;
+    }
+
+    private class BitmapCache implements ImageLoader.ImageCache {
+
+        private LruCache<String, Bitmap> mMemoryCache;
+
+        public BitmapCache() {
+            int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+            int cacheSize = maxMemory / 8;
+
+            mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+                @Override
+                protected int sizeOf(String key, Bitmap bitmap) {
+                    return bitmap.getByteCount() / 1024;
+                }
+            };
+        }
+
+        @Override
+        public Bitmap getBitmap(String s) {
+            return mMemoryCache.get(s);
+        }
+
+        @Override
+        public void putBitmap(String s, Bitmap bitmap) {
+            if (mMemoryCache.get(s) == null) {
+                mMemoryCache.put(s, bitmap);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/star/listviewtest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.star.listviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.list_view);
+        ImageAdapter imageAdapter = new ImageAdapter(this, 0, Image.IMAGE_URLS);
+
+        mListView.setAdapter(imageAdapter);
+    }
+}
